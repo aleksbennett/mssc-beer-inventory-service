d0d4ae41e9f504622a2f28b3c10aa955b22fc0f5
Bennett <Aleks.Bennett@bregroup.com>
2020-05-14 11:50:39 +0100

    Implemented BOM in POM

--- src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInvntoryBootstrap.java
+++ src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInvntoryBootstrap.java
@@ -19,9 +19,9 @@ public class BeerInvntoryBootstrap implements CommandLineRunner {
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
-    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
+    public static final UUID BEER_1_UUID = UUID.fromString("e2acab14-e884-4fe8-acb5-36060056f271");
+    public static final UUID BEER_2_UUID = UUID.fromString("97db0991-cfa5-4756-a935-0c53a01dc407");
+    public static final UUID BEER_3_UUID = UUID.fromString("b1844100-62e3-4cb4-916f-df95d7a9ec09");
 
     private final BeerInventoryRepository beerInventoryRepository;
 
@@ -44,14 +44,14 @@ public class BeerInvntoryBootstrap implements CommandLineRunner {
                 .builder()
                 .beerId(BEER_2_UUID)
                 .upc(BEER_2_UPC)
-                .quantityOnHand(50)
+                .quantityOnHand(51)
                 .build());
 
         beerInventoryRepository.saveAndFlush(BeerInventory
                 .builder()
                 .beerId(BEER_3_UUID)
                 .upc(BEER_3_UPC)
-                .quantityOnHand(50)
+                .quantityOnHand(52)
                 .build());
 
         log.debug("Loaded Inventory. Record count: " + beerInventoryRepository.count());
